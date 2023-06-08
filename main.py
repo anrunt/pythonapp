1563ecf55ae38748d192757e5815e9209111dc68
anrunt <75736769+anrunt@users.noreply.github.com>
2023-06-08 19:10:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+import random
+
+def randomcode():
+    code_length = entry_letters_int.get()
+    code = list(''.join(random.choice("ABCDEFGHIJKLMNOPRSTUWabcdefghijklmnoprstuwz1234567890!@#$%^&*")for i in range(code_length))) 
+    random.shuffle(code)
+
+    output_string.set(code)
+    
+
+#window
+
+window = ttk.Window(themename='darkly')
+window.title = "Anrunt's code generator"
+window.geometry("300x190")
+
+#title 
+
+title_label = ttk.Label(
+    master = window, 
+    text = "Code Generator", 
+    font = 'Calibri 20 bold')
+
+how_many_Label = ttk.Label(
+    master = window,
+    text = 'Enter code length:',
+    font = 'Calibri 12' )
+
+title_label.pack()
+how_many_Label.pack()
+
+#input field
+
+input_letters_frame = ttk.Frame(master = window)
+
+
+entry_letters_int = tk.IntVar()
+enter_letters = ttk.Entry(
+    master = input_letters_frame,
+    textvariable = entry_letters_int)
+
+
+button = ttk.Button(
+    master = input_letters_frame, 
+    text = "Generate",
+    command = randomcode )
+
+enter_letters.pack()
+button.pack(pady=10)
+
+
+input_letters_frame.pack()
+
+
+#output
+
+output_string = tk.StringVar()
+output_Label = ttk.Label(
+    master = window,
+    text = 'Output',
+    font = 'Calibri 15',
+    textvariable = output_string
+)
+output_Label.pack(pady=5)
+
+#run 
+
+window.mainloop()
