9bb67d2d3d77ed8d7bddbdd5a51b638d58ecde73
anrunt <75736769+anrunt@users.noreply.github.com>
2023-07-08 16:04:49 +0200

    Update weatherapp.py
    Converted app using to use class approach

--- weatherapp.py
+++ weatherapp.py
@@ -4,75 +4,109 @@ from tkinter import ttk
 import math
 from tkinter import messagebox
 from PIL import Image, ImageTk
-import ttkbootstrap
-
+import ttkbootstrap as ttk
 
 def get_weather(city):
-    API_KEY = '862957c16730a84ba992b581fadd84e4'
-    BASE_URL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+        API_KEY = '862957c16730a84ba992b581fadd84e4'
+        BASE_URL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
 
-    result = requests.get(BASE_URL)
+        result = requests.get(BASE_URL)
+    
 
-    if result.status_code == 404:
-        messagebox.showerror("Error", "City not found")
-        return None
+        if result.status_code == 404:
+            messagebox.showerror("Error", "City not found")
+            return None
 
-    weather = result.json()
-    temperature = weather['main']['temp'] - 273.15
-    city = weather['name']
-    sky = weather['weather'][0]['main']
-    icon_id = weather['weather'][0]['icon']
-
-    # importing image
-    icon_url = f"http://openweathermap.org/img/wn/{icon_id}@2x.png"
-
-    return (city, temperature, sky, icon_url)
-
+        weather = result.json()
+        temperature = weather['main']['temp'] - 273.15
+        city = weather['name']
+        sky = weather['weather'][0]['description']
+        icon_id = weather['weather'][0]['icon']
+        humidity = weather['main']['humidity']
+        windspeed = weather['wind']['speed']
+        # importing image
+        icon_url = f"http://openweathermap.org/img/wn/{icon_id}@2x.png"
+    
+        return (city, temperature, sky, icon_url, humidity, windspeed)
 
 def search():
-    city = entry.get()
-    result = get_weather(city)
+        
+        city = entry.get()
+        result = get_weather(city)
 
-    (city, temperature, sky, icon_url) = result # type:ignore 
+        (city, temperature, sky, icon_url, humidity, windspeed) = result # type:ignore 
 
-    image = Image.open(requests.get(icon_url, stream=True).raw)
-    icon = ImageTk.PhotoImage(image)
-    icon_label.configure(image=icon)
-    icon_label.image = icon # type:ignore
+        image = Image.open(requests.get(icon_url, stream=True).raw)
+        icon = ImageTk.PhotoImage(image)
+        icon_label.configure(image=icon)
+        icon_label.image = icon # type:ignore
 
-    round_temp = round(temperature, 1)
+        round_temp = round(temperature, 1)
 
-    city_name_label.configure(text=city)
+        city_name_label.configure(text=city)
 
-    temperature_label.configure(text=f"Temperature: {round_temp}°C")
+        temperature_label.configure(text=f"Temperature: {round_temp}°C")
 
-    appearance_label.configure(text=sky)
+        appearance_label.configure(text=sky)
+
+        humidity_label.configure(text=f"Humidity: {humidity}%")
+
+        wind_speed_label.configure(text=f"Wind Speed: {windspeed} m/s")
+        
 
 
-# main
-window = ttkbootstrap.Window(themename="vapor")
-window.geometry('400x500')
-window.title('Cloudly')
 
-# Entry
-entry = tk.Entry(window, font=('Arial', 22))
-entry.pack(pady=5)
+class App(ttk.Frame):
+    def __init__(self,parent):
+        super().__init__(parent)
+        self.pack(expand=True,fill='both')
 
-# Button
-search_button = tk.Button(window,height=2,width=20, text='Search', command=search)
-search_button.pack(pady=5)
+        global entry
+        global city_name_label
+        global temperature_label
+        global icon_label
+        global appearance_label
+        global humidity_label
+        global wind_speed_label
 
-city_name_label = tk.Label(window, text="", font=('Arial', 20))
-city_name_label.pack(pady=10)
+        # search your city label
+        ttk.Label(self, text='Enter your city name: ',font=('Arial', 20)).pack()
 
-temperature_label = tk.Label(window, text="", font=('Arial', 20))
-temperature_label.pack(pady=10)
+        # search
+        entry = tk.Entry(self,font=('Arial', 22))
+        entry.pack(pady=10)
 
-icon_label = tk.Label(window)
-icon_label.pack(pady=10)
+        # button
+        search_button = tk.Button(self,height=2,width=20, text='Search',command=search)
+        search_button.pack(pady=5)
 
-appearance_label = tk.Label(window, text="", font=('Arial', 20))
-appearance_label.pack(pady=10)
+        # labels 
+        city_name_label = tk.Label(self, text="", font=('Arial', 20))
+        city_name_label.pack(pady=10)
 
-# run
-window.mainloop()
+        temperature_label = tk.Label(self, text="", font=('Arial', 20))
+        temperature_label.pack(pady=10)
+
+        icon_label = tk.Label(self)
+        icon_label.pack()
+
+        appearance_label = tk.Label(self, text="", font=('Arial', 20))
+        appearance_label.pack()
+
+        humidity_label = tk.Label(self,text="",font=('Arial',13))
+        humidity_label.place(relx=0.082,rely=0.9)
+
+        wind_speed_label = tk.Label(self,text='',font=('Arial',13))
+        wind_speed_label.place(relx=0.5, rely=0.9)
+
+        
+
+# main 
+weatherapp = ttk.Window('Cloudly',themename='vapor')
+weatherapp.geometry('400x500')
+
+# widgets
+App(weatherapp)
+
+# run 
+weatherapp.mainloop()
